0bb02f5cc79c229931f2530a759079a9ec66f4ab
Uqba-Usman <57703614+Uqba-Usman@users.noreply.github.com>
2020-08-21 23:49:08 +0500

    .

--- connection/db.js
+++ connection/db.js
@@ -14,14 +14,14 @@ var mysql = require("mysql");
 // });
 function newConn() {
   var connection = mysql.createConnection({
-    // host: "us-cdbr-east-02.cleardb.com",
-    // user: "b96e10c55848d3",
-    // password: "a4ce1d56",
-    // database: "heroku_4d2072f812ae958",
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "project_book_store",
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "b96e10c55848d3",
+    password: "a4ce1d56",
+    database: "heroku_4d2072f812ae958",
+    // host: "localhost",
+    // user: "root",
+    // password: "",
+    // database: "project_book_store",
     // debug: true,
   });
 
